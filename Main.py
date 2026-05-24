f58f0e7b5cb3bbbd7e400ad98aa3a1767e0bdb6c
Arata Nakashima/中島　新大 <aratan0418@outlook.jp>
2026-05-24 12:51:37 +0900

    Main.py

--- Main.py
+++ Main.py
@@ -1,10 +1,33 @@
 num1 = int(input("type in number:"))
+def calc1():
 num2 = int(input("type in second number"))
+  num1 = int(input("type in no1"))
 add = num1 + num2
+  num2 = int(input("type in no2"))
 sub = num1 - num2
+  if op == "+":
 mul = num1 * num2
+    print(num1 + num2)
 div = num1 / num2
+  elif op == "-":
 print("addition:"+str(add))
+    print(num - num2)
 print("subtraction:"+str(sub))
+  elif op == "*":
 print("multiplication:"+str(mul))
+    print(num1*num2)
 print("division:"+str(div))
+def calc1():
+
+  num1 = int(input("type in no1"))
+  num2 = int(input("type in no2"))
+  if op == "+":
+    print(num1 + num2)
+  elif op == "-":
+    print(num - num2)
+  elif op == "*":
+    print(num1*num2)
+  elif op== "/":
+    print(num1/num2)
+  else:
+    print("error")
