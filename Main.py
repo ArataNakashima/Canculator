a740e53505574ce0a7fd8ac7c83031d290a36471
Arata Nakashima/中島　新大 <aratan0418@outlook.jp>
2026-05-24 12:31:10 +0900

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,10 @@
+num1 = int(input("type in number:"))
+num2 = int(input("type in second number"))
+add = num1 + num2
+sub = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+print("addition:"+str(add))
+print("subtraction:"+str(sub))
+print("multiplication:"+str(mul))
+print("division:"+str(div))
